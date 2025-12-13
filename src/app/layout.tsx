a49e9ef1645192037b7ac7407bd31eba0994574d
Ahmad Afifuddin <ahmadafifudding@gmail.com>
2025-12-13 11:14:20 +0800

    feat(layout): add Georama font and update background image
    Add Georama font variable to body class and change wallpaper.png to wallpaper.jpg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next"
-import { Geist, Geist_Mono } from "next/font/google"
+import { Geist, Geist_Mono, Georama } from "next/font/google"
 import "./globals.css"
 import type { ReactNode } from "react"
 import { Navbar } from "@/components/navbar"
@@ -11,6 +11,11 @@ const geistSans = Geist({
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
+  subsets: ["latin"],
+})
+
+const georama = Georama({
+  variable: "--font-georama",
   subsets: ["latin"],
 })
 
@@ -27,7 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} h-dvh w-dvw overflow-hidden bg-[url(/images/wallpaper.png)] bg-center bg-cover bg-no-repeat antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${georama.variable} h-dvh w-dvw overflow-hidden bg-[url(/images/wallpaper.jpg)] bg-center bg-cover bg-no-repeat antialiased`}
       >
         <Navbar />
         {children}
